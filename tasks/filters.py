173e9bc02a801e5d16b76350c9f466375a5d5f4a
Miguel Angelo <miguelrsant01@gmail.com>
2026-06-27 15:53:31 +0000

    feat: add task filtering by status and title

--- tasks/filters.py
+++ tasks/filters.py
@@ -6,15 +6,17 @@ from rest_framework.exceptions import ValidationError
 def filter_tasks(tasks, request):
 
     status = request.query_params.get("status")
+    title = request.query_params.get("title")
 
-    if not status:
-        return tasks
-
-    if status not in Task.Status.values:
+    if status and status not in Task.Status.values:
         raise ValidationError(
             {
                 "message": "Status inválido."
             }
         )
+    if status:
+        tasks = tasks.filter(status=status)
+    if title:
+        tasks = tasks.filter(title__icontains=title)
 
-    return tasks.filter(status=status)
+    return tasks
