7345bc0e8cc0bcc814059441a9cce3f088630cc2
Miguel Angelo <miguelrsant01@gmail.com>
2026-06-27 08:54:51 +0000

    feat: add task CRUD with user authentication

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskView
+from .views import TaskView, TaskDetailView
 
 urlpatterns = [
     path("", TaskView.as_view()),
+    path("<int:id>/", TaskDetailView.as_view()),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from .models import Task
 from .serializers import TaskSerializer
-
+import django.utils.timezone as timezone
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -37,9 +37,91 @@ class TaskView(APIView):
 
         serializer.is_valid(raise_exception=True)
 
-        taks = serializer.save(user=request.user)
+        task = serializer.save(user=request.user)
 
         return Response(
-            TaskSerializer(taks).data,
+            TaskSerializer(task).data,
             status=201
+        )
+
+
+class TaskDetailView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id):
+
+        try:
+            task = Task.objects.get(
+                id=id,
+                user=request.user,
+                deleted_at__isnull=True
+            )
+
+        except Task.DoesNotExist:
+            return Response(
+                {
+                    "message": "Tarefa não encontrada."
+                },
+                status=404
+            )
+
+        serializer = TaskSerializer(task)
+
+        return Response(serializer.data)
+
+    def patch(self, request, id):
+
+        try:
+            task = Task.objects.get(
+                id=id,
+                user=request.user,
+                deleted_at__isnull=True
+            )
+
+        except Task.DoesNotExist:
+            return Response(
+                {
+                    "message": "Tarefa não encontrada."
+                },
+                status=404
+            )
+
+        serializer = TaskSerializer(
+            task,
+            data=request.data,
+            partial=True
+        )
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+
+        try:
+            task = Task.objects.get(
+                id=id,
+                user=request.user,
+                deleted_at__isnull=True
+            )
+
+        except Task.DoesNotExist:
+            return Response(
+                {
+                    "message": "Tarefa não encontrada."
+                },
+                status=404
+            )
+
+        task.deleted_at = timezone.now()
+        task.save()
+
+        return Response(
+            {
+                "message": "Tarefa removida com sucesso."
+            },
+            status=200
         )
