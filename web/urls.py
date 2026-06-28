dd3d16957fdbfbd0ac44bf3152491fdfd09f6327
Miguel Angelo <miguelrsant01@gmail.com>
2026-06-28 14:34:36 +0000

    feat: add home page

--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_page, name="login"),
+    path("", views.home_page, name="home"),
+    path("login/", views.login_page, name="login"),
     path("register/", views.register_page, name="register"),
     path("dashboard/", views.dashboard, name="dashboard"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def home_page(request):
+    return render(request, "home.html")
+
 
 def login_page(request):
     return render(request, "login.html")
